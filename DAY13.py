31b8dd1322f507af1d4c1884b9857e469bb71a61
duncandegwa <74187010+duncandegwa@users.noreply.github.com>
2022-09-22 23:53:33 +0300

    CLASS

---
+++ DAY13.py
@@ -0,0 +1,17 @@
+class Rabbit():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def setname(self):
+        print("MY NAME IS " + self.name)
+jina=input("ENTER NAME")
+agea=input("ENTER NAME")
+
+class Dlack(Rabbit):
+    super().__init__(self,name,age)
+    def clolor(self,clor):
+        print("is black in colur")
+jina=input("ENTER NAME")
+agea=input("ENTER NAME")
+califonia=Dlack("GITONTO","12")
+califonia.setname()
